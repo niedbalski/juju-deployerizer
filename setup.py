b002d3817bb2a38bb54e223502b6d903aa4cd652
Jorge Niedbalski <jnr@metaklass.org>
2015-03-18 15:01:51 -0300

    output default to stdout
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependencies = ["PyYaml"]
 
 setup(
     name="juju-deployerizer",
-    version="0.1.2",
+    version="0.1.3",
     author="Jorge Niedbalski R.",
     include_package_data=True,
     author_email="jnr@metaklass.org",
